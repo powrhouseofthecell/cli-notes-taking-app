39a0ca04306eae2af723378030d90d97d10a1a0e
Zuhaib <theshahzuhaib96@gmail.com>
2022-01-13 16:24:56 +0530

    New Add

---
+++ README.md
@@ -0,0 +1,49 @@
+<h1 align="center"> Notes App 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: ISC" src="https://img.shields.io/badge/License-ISC-yellow.svg" />
+  </a>
+</p>
+
+> A CLI notes taking app
+
+## Install
+
+```sh
+npm install
+```
+
+## Run The Script
+
+```sh
+npm run start
+```
+
+## Add a new note
+
+node app.js add --title="Buy" --body="Food"
+
+## List all note
+
+node app.js list
+
+## Read a note
+
+node app.js read --title="Buy"
+
+## Remove a note
+
+node app.js remove --title="Buy"
+
+## Author
+
+👤 **Zuhaib Nazir**
+
+-  Website: [zuhaib.live](https:/zuhaib.live)
+-  Github: [@powrhouseofthecell](https://github.com/powrhouseofthecell)
+-  LinkedIn: [@zuhaibnazir](https://linkedin.com/in/zuhaibnazir)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
---
+++ app.js
@@ -0,0 +1,72 @@
+// process.argv;
+const yargs = require("yargs")
+const notes = require("./notes.js")
+
+// Customize yargs version
+yargs.version("1.1.0")
+
+// Create add command
+yargs.command({
+	command: "add",
+	describe: "Add a new note",
+	// options are specified in builder
+	builder: {
+		title: {
+			describe: "Note title",
+			demandOption: true,
+			type: "string",
+		},
+		body: {
+			describe: "Note body",
+			demandOption: true,
+			type: "string",
+		},
+	},
+	// yargs calls the handler function with the argv argument that contains the arguments.
+	handler: argv => {
+		notes.addNote(argv.title, argv.body)
+	},
+})
+
+// Create remove command
+yargs.command({
+	command: "remove",
+	describe: "Remove a note",
+	builder: {
+		title: {
+			describe: "Remove a note",
+			demandOption: true,
+			type: "string",
+		},
+	},
+	handler: argv => {
+		notes.removeNote(argv.title)
+	},
+})
+
+// Create list command
+yargs.command({
+	command: "list",
+	describe: "List your notes",
+	handler: () => {
+		notes.listNotes()
+	},
+})
+
+// Create read command
+yargs.command({
+	command: "read",
+	describe: "Read a note",
+	builder: {
+		title: {
+			describe: "Read a note",
+			demandOption: true,
+			type: "string",
+		},
+	},
+	handler: argv => {
+		notes.readNote(argv.title)
+	},
+})
+
+yargs.parse()
